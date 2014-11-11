368f16441048175d51b427e591e030b7374a4b3b
Kelly Selden <kellyselden@gmail.com>
2014-11-10 20:09:53 -0500

    code added

---
+++ ExcelAbstraction.NPOI/ExcelService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ExcelAbstraction.Entities;
+using ExcelAbstraction.Services;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace ExcelAbstraction.NPOI
+{
+	public class ExcelService : IExcelService
+	{
+		public Workbook ReadWorkbook(string path)
+		{
+			if (!File.Exists(path)) return null;
+
+			using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+				return CreateWorkbook(new HSSFWorkbook(stream));
+		}
+
+		static Workbook CreateWorkbook(IWorkbook workbook)
+		{
+			var worksheets = new List<Worksheet>();
+			for (int i = 0; i < workbook.NumberOfSheets; i++)
+				worksheets.Add(CreateWorksheet(workbook.GetSheetAt(i)));
+			return new Workbook(worksheets);
+		}
+
+		static Worksheet CreateWorksheet(ISheet sheet)
+		{
+			var rows = new List<IRow>();
+			int maxColumns = 0;
+			for (int i = 0; i < sheet.PhysicalNumberOfRows; i++)
+			{
+				IRow row = sheet.GetRow(i);
+				maxColumns = Math.Max(maxColumns, row.LastCellNum);
+				rows.Add(row);
+			}
+			return new Worksheet(sheet.SheetName, maxColumns, rows.Select(row => CreateRow(row, maxColumns)));
+		}
+
+		static Row CreateRow(IRow row, int columns)
+		{
+			var cells = new List<Cell>();
+			ICell[] iCells = row.Cells.ToArray();
+			int skipped = 0;
+			for (int i = 0; i < columns; i++)
+			{
+				Cell cell;
+				if (i - skipped >= iCells.Length || i != iCells[i - skipped].ColumnIndex)
+				{
+					cell = new Cell(row.RowNum, i, null);
+					skipped++;
+				}
+				else cell = CreateCell(iCells[i - skipped]);
+				cells.Add(cell);
+			}
+			return new Row(cells);
+		}
+
+		static Cell CreateCell(ICell cell)
+		{
+			string value = null;
+			switch (cell.CellType)
+			{
+				case CellType.String:
+					value = cell.StringCellValue;
+					break;
+				case CellType.Numeric:
+					value = cell.NumericCellValue.ToString();
+					break;
+				case CellType.Formula:
+					switch (cell.CachedFormulaResultType)
+					{
+						case CellType.String:
+							value = cell.StringCellValue;
+							break;
+						case CellType.Numeric:
+							//excel trigger is probably out-of-date
+							value = (cell.CellFormula == "TODAY()" ? DateTime.Today.ToOADate() : cell.NumericCellValue).ToString();
+							break;
+					}
+					break;
+			}
+			return new Cell(cell.RowIndex, cell.ColumnIndex, value);
+		}
+	}
+}
---
+++ ExcelAbstraction.NPOI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExcelAbstraction.NPOI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ExcelAbstraction.NPOI")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bf1df2c6-7361-488c-bf9e-3bb3f9c3df98")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
